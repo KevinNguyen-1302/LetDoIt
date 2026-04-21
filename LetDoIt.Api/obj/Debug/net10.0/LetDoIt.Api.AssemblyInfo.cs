f77f444e5734a4de87554db0bd9a22041cef9b10
KevinNguyen-1302 <duykhang.personal@gmail.com>
2026-04-21 16:03:44 +0700

    [add] gitignore

--- LetDoIt.Api/obj/Debug/net10.0/LetDoIt.Api.AssemblyInfo.cs
+++ LetDoIt.Api/obj/Debug/net10.0/LetDoIt.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LetDoIt.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3982a7e1d14cdf2bc2f5d1e31b0f61d7b181268f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb036396786091015dfd5b93d14c8644c0e4e64f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LetDoIt.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LetDoIt.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
