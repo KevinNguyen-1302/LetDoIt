18988f151df31711a038497623da71a3b2603fe0
KevinNguyen-1302 <duykhang.personal@gmail.com>
2026-05-04 17:29:34 +0700

    [ADD] Session service

---
+++ LetDoIt.Api/DTOs/CreateSessionRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LetDoIt.Api.DTOs
+{
+    public class CreateSessionRequestDto
+    {
+        public Guid? TaskId { get; set; }
+
+        [Required]
+        public DateTime StartTime { get; set; }
+
+        [Required]
+        public DateTime EndTime { get; set; }
+    }
+}
--- LetDoIt.Api/Models/Session.cs
+++ LetDoIt.Api/Models/Session.cs
@@ -6,27 +6,25 @@ namespace LetDoIt.Api.Models;
 public class Session
 {
     [Key]
-    public int SessionId { get; set; }
-
+    public Guid SessionId { get; set; }
     [Required]
-    [ForeignKey("Task")]
-    public Guid TaskId { get; set; }
+    public Guid UserId { get; set; }
+    public Guid? TaskId { get; set; }
 
     [Required]
     public DateTime StartTime { get; set; } = DateTime.UtcNow; // Mặc định là thời điểm tạo Session
-
-    // EndTime có thể cho phép Null (?) vì khi một phiên Pomodoro vừa bắt đầu, 
-    // nó chưa có thời gian kết thúc.
     public DateTime? EndTime { get; set; } 
 
     // Tính bằng phút (Int). Sẽ được cập nhật khi Session kết thúc.
     public int Duration { get; set; } 
-
     [Required]
     [StringLength(20)] // Tương ứng với Nvarchar(20) trong database
     public SessionStatus Status { get; set; } = SessionStatus.Running; // Gán giá trị mặc định khi khởi tạo
-    
+    [ForeignKey("TaskId")]
     public virtual Task? Task { get; set; }
+    [ForeignKey("UserId")]
+
+    public virtual Users? User { get; set; }
 }
 
 public enum SessionStatus
